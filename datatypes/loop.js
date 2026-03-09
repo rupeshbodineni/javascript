aafbfbcdfc933f765bb62d50e3784f9e15167616
rupeshbodineni <rupeshbodineni@gmail.com>
2026-03-09 11:19:01 +0530

    four

---
+++ datatypes/loop.js
@@ -0,0 +1,10 @@
+for(let i=1;i<=10;i++){
+    console.log(i);
+}
+//when you know the fixed number of iterations
+
+
+let enames=["rupesh","suresh","mahesh"];
+for(let i=0;i<enames.length-1;i++){
+    console.log(enames[i]);
+}
---
+++ datatypes/while.js
@@ -0,0 +1,13 @@
+//let i=1;
+//while(i<=10){
+  //  console.log(i);
+   // i++;
+//}
+//if you dont know about no of iterations
+
+let i=0;
+let enames=["rupesh","suresh","mahesh"];
+while(i<=enames.length-1){
+    console.log(enames[i]);
+    i++;
+}
