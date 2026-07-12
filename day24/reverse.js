f254298e192b0623177c33ee62d4ede7340dc386
rupeshbodineni <rupeshbodineni@gmail.com>
2026-07-12 21:33:54 +0530

    R

---
+++ day24/reverse.js
@@ -0,0 +1,22 @@
+function reversestring(str){
+     let reverse=" "
+     for (let i=str.length-1;i>=0;i--){
+        reverse=reverse+str[i]
+     }
+     return reverse
+}
+ console.log(reversestring("hello"))
+
+ function reversestring(str){
+    let output=""
+    for(let i=str.length-1;i>=1;i--){
+        output=output+str[i]
+    }
+    return output
+ }
+ console.log(reversestring("rahul"))
+
+
+ function reversestring(str){
+       let 
+ }
